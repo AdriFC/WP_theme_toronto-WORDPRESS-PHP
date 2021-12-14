d67deb968c3fe91dc75e4a986d6d071379856685
AdriFC <adri86vigo@gmail.com>
2021-12-14 17:17:51 +0100

    Detectar cuando no hay imagen destacada.

--- wp-content/themes/toronto/page.php
+++ wp-content/themes/toronto/page.php
@@ -2,10 +2,17 @@
 
     <?php while(have_posts() ): the_post(); ?>
 
+    <!-- Detectar si hay imagen destacada -->
+    <?php if (has_post_thumbnail()) { ?>
+
         <div class="destacada">
             <?php the_post_thumbnail('destacada'); ?>
             <h2><?php the_title(); ?></h2>
         </div>
+
+    <?php } else { ?>
+        <h2 class="noimagen"><?php the_title(); ?></h2>
+    <?php } ?>
 
         <div id="primary" class="primary">
             <?php the_content(); ?>
